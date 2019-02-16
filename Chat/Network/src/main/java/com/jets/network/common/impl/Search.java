e907a276742e0c46896f5f3f0389ae39dc2a156b
Nouran Amr <nouranamr206@gmail.com>
2019-02-16 10:25:25 +0200

    List<Friend> instead of User in searchUser Method In SearchInt List<Group> instead of Group in searchGroup Method In SearchInt Implement Search and User Settings in common.impl Add RegisterForm and LoginForm

---
+++ Chat/Network/src/main/java/com/jets/network/common/impl/Search.java
@@ -0,0 +1,38 @@
+package com.jets.network.common.impl;
+
+import java.rmi.RemoteException;
+import java.util.List;
+
+import com.jets.database.dal.dao.impl.FriendsDao;
+import com.jets.database.dal.dao.impl.GroupDao;
+import com.jets.database.dal.dto.Friend;
+import com.jets.database.dal.dto.Group;
+import com.jets.network.common.serverservice.SearchInt;
+
+/**
+  *
+ * @author Nouran Amr
+ *
+ */
+public class Search implements SearchInt {
+
+	FriendsDao friend;
+	GroupDao group;
+	@Override
+	public List<Friend>  searchUser(String search) throws RemoteException {
+		
+		
+		List<Friend> friendList=friend.retrieveByName(search);
+		
+		return friendList;
+	}
+
+	@Override
+	public List<Group> searchGroup(String search) throws RemoteException {
+		
+		List<Group>groupList=group.retrieveByName(search);
+		
+		return groupList;
+	}
+
+}
